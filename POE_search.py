960c04380edc2324e141f0504b1cf76878779217
michael-himes <37747857+michael-himes@users.noreply.github.com>
2020-04-20 19:25:40 -0600

    Added base and link +/- 1 specific search

--- POE_search.py
+++ POE_search.py
@@ -3,7 +3,6 @@ import requests
 import json
 
 
-start =  timer()
 content = []
 iteration = 0
 interval = 0
@@ -15,35 +14,8 @@ while True:
         last = timer()
         iteration += 1
         content.append(line)
-    #if line == 'qq':
-    #    break
     else:
         break
-
-cookies = {
-    '_ga': 'GA1.2.1454003851.1584649422',
-    '__qca': 'P0-1838013029-1584649422698',
-    '__gads': 'ID=cddbdb8d500fcb86:T=1584649426:S=ALNI_MY2_uIdaxoqOeNrip3jgAR9xyA7Qw',
-    'league': 'Delirium',
-    'CRISPSUBNO': '3c6f0bc0d631048ac3b3a4e2ffa2be34',
-    '_gid': 'GA1.2.151124699.1586906311',
-}
-
-headers = {
-    'Connection': 'keep-alive',
-    'Cache-Control': 'max-age=0',
-    'Origin': 'https://poe.trade',
-    'Upgrade-Insecure-Requests': '1',
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
-    'Sec-Fetch-Dest': 'document',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'Sec-Fetch-Site': 'same-origin',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-User': '?1',
-    'Referer': 'https://poe.trade/search/ehamewaritanoh',
-    'Accept-Language': 'en-US,en;q=0.9',
-}
 
 data = {
   'league': 'Delirium',
@@ -135,11 +107,21 @@ data = {
   'has_buyout': '',
   'exact_currency': ''
 }
+print() # i needs space
 name = content[1]
+base = content[2]
+for element in content:
+    if "Sockets" in element:
+        linked = element.count('-')+1
+        data['link_min'] = linked - 1
+        data['link_max'] = linked + 1
 print(name)
+print(base)
+print(linked, "links +/- 1")
 data['name'] = name
+data['base'] = base
 
-response = requests.post('https://poe.trade/search', headers=headers, cookies=cookies, data=data)
+response = requests.post('https://poe.trade/search',  data=data)
 
 curency = {}
 for line in response.text.split('\n'):
@@ -151,3 +133,5 @@ for line in response.text.split('\n'):
             curency[value] += 1
 for item in curency:
     print('\t',curency[item],'\t',item)
+print() # i needs space
+print(response.url)
